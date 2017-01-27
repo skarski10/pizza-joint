17070cf81b1b1d537c62799a0c11db5b596ce250
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-27 11:55:14 -0800

    add

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,23 @@ var orderPrice;
 function Order (size, toppings) {
   this.sizeOrdered = size;
   this.toppingsOrdered = toppings;
-  // this.orderPrice;
   console.log(this.sizeOrdered);
   console.log(this.toppingsOrdered);
 }
+var defaultPrice = 14;
 
-
+function toppings() {
+  defaultPrice = defaultPrice + 0.5;
+  return defaultPrice;
+}
+console.log(defaultPrice);
 Order.prototype.totalPrice = function() {
-  var defaultPrice = 14.00;
-  function toppings(){
-    defaultPrice = defaultPrice + 0.5;
-    return defaultPrice;
-  }
+  console.log(this.sizeOrdered);
+  console.log(toppingsOrdered);
   toppingsOrdered.forEach(toppings);
-  if (sizeOrdered === 'small') {
+  if (size === 'small') {
     return defaultPrice -2;
-  } else if (sizeOrdered === 'large') {
+  } else if (size === 'large') {
     return defaultPrice +3
   }
   return defaultPrice;
@@ -34,5 +35,7 @@ $(document).ready(function() {
     });
     var pizzaSize = $("input:radio[name=size]:checked").val();
   var order = new Order(pizzaSize, toppingsChecked);
+
+  // $('#pizza-cost').text(order.totalPrice);
   });
 });
