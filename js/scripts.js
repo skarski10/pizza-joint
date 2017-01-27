61296f9900bcc78a2bf8f3aa215bcda1a8fceadd
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-27 14:08:02 -0800

    add meat toppings to the form

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,16 @@ function veggies () {
   defaultPrice = defaultPrice + 0.5;
   return defaultPrice;
 }
+
+function meats () {
+  defaultPrice = defaultPrice + 1;
+  return defaultPrice;
+}
 Order.prototype.totalPrice = function() {
   defaultPrice = 14;
   order.toppingsOrdered.forEach(veggies)
+  order.toppingsOrdered.forEach(meats)
+
   if (order.sizeOrdered === 'small') {
     return defaultPrice -2;
   } else if (order.sizeOrdered === 'large') {
@@ -32,6 +39,9 @@ $(document).ready(function() {
     var pizzaSize = $("input:radio[name=size]:checked").val();
     order;
     order = new Order(pizzaSize, toppingsChecked);
-  $('#pizza-cost').text(order.totalPrice);
+    $('#hide-me').hide();
+    $('#hide-me').show();
+    $('#pizza-cost').text(order.totalPrice);
+
   });
 });
