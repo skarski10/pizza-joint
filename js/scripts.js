ad13db888ba53634b2a7efdcff5742b465bfd22a
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-27 10:36:43 -0800

    add to prototype totalPrice to adjust price for size and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 // business logic
+var orderPrice;
 function Order (size, toppings) {
   this.sizeOrdered = size;
   this.toppingsOrdered = toppings;
+  // this.orderPrice;
 }
 
-Order.prototype.totalPrice = function(){
+Order.prototype.totalPrice = function() {
   var defaultPrice = 14.00;
+  function toppings(){
+    defaultPrice = defaultPrice + 0.5;
+    return defaultPrice;
+  }
+  toppingsOrdered.forEach(toppings);
+  // orderPrice = defaultPrice;
+
   if (sizeOrdered === 'small') {
     return defaultPrice -2;
-  } else if (sizeordered === 'Large') {
+  } else if (sizeOrdered === 'large') {
     return defaultPrice +3
   }
+  return defaultPrice;
 }
 // user logic
