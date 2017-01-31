e6c1d75ea84dabebf50bded7d4441b7bc0f097b7
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-30 22:15:27 -0800

    change styling to jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 // business logic
 var order;
 var defaultPrice;
-function Order (size, veggies, meats) {
+function Order (size, veggies, meats, specialty) {
   this.sizeOrdered = size;
   this.veggiesOrdered = veggies;
   this.meatsOrdered = meats;
+  this.specialtyOrdered = specialty;
 }
 
 function veggies () {
@@ -16,10 +17,18 @@ function meats () {
   defaultPrice = defaultPrice + 1;
   return defaultPrice;
 }
+
+function specialty () {
+  defaultPrice = defaultPrice + 1.50;
+  return defaultPrice;
+}
+
 Order.prototype.totalPrice = function() {
   defaultPrice = 14;
   order.veggiesOrdered.forEach(veggies)
   order.meatsOrdered.forEach(meats)
+  order.specialtyOrdered.forEach(specialty)
+
 
   if (order.sizeOrdered === 'small') {
     return defaultPrice -2;
@@ -35,19 +44,23 @@ $(document).ready(function() {
     event.preventDefault();
     var veggiesChecked = [];
     var meatsChecked = [];
+    var specialtyChecked = [];
     $("input:checkbox[name=veggies]:checked").each(function(){
       veggiesChecked.push($(this).val());
     });
     $("input:checkbox[name=meats]:checked").each(function(){
       meatsChecked.push($(this).val());
     });
+    $("input:checkbox[name=specialty]:checked").each(function(){
+      specialtyChecked.push($(this).val());
+    });
     var pizzaSize = $("input:radio[name=size]:checked").val();
     order;
-    order = new Order(pizzaSize, veggiesChecked, meatsChecked);
-    $('.orders').hide();
+    order = new Order(pizzaSize, veggiesChecked, meatsChecked, specialtyChecked);
+    $('#ordering').hide();
     $('.hide-me').show();
     $('#pizza-cost').text(order.totalPrice);
-    $('#pizza-ordered').text('1 ' + order.sizeOrdered + ' pizza with ' + order.veggiesOrdered + ' & ' + order.meatsOrdered);
+    $('#pizza-ordered').text(order.sizeOrdered + ' pizza with ' + order.veggiesOrdered + ' & ' + order.meatsOrdered);
 
   });
 });
